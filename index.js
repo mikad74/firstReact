1415518ca6bebc859212188a60adb3a72789054b
Mika Doorenbosch <mika.doorenbosch@gmail.com>
2022-11-14 20:10:24 +0100

    navbar

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const page = (
+  <nav>
+    <h1>React</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(page
+  , document.getElementById("root"))
