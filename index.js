5072dcf58a330b6808ad9fb7a3f451e6df4265c5
mikad74 <mika.doorenbosch@gmail.com>
2022-11-15 22:31:40 +0100

    f

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-
-function Navbar() {
+function Header() {
   return (
-  <header> 
-    <nav>
-      <img src="./React-icon.svg.png" width="40px"/>
-      <h1>Navbar</h1>
-      <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  </header>
+    <header>
+      <nav className="nav-bar">
+        <img src="./React-icon.svg.png" />
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Title() {
+  return <h1>React is cool</h1>;
+}
+function List() {
+  return (
+    <ol>
+      <li>It's composable</li>
+      <li>React is declerative</li>
+      <li>React is being maintained by my favourite company</li>
+      <li>Seems to make you hireable</li>
+      <li>Looks cool</li>
+    </ol>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Doorenbosch development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+      <React.Fragment>
+      <Header />
+      <div className = "main">
+        <Title />
+        <List />
+      </div>
+      <Footer />
+      </React.Fragment>
   )
 }
 
-function List() {
-return (
-  <ol>
-    <li>It's composable</li>
-    <li>React is declerative</li>
-    <li>React is being maintained by my favourite company</li>
-    <li>Seems to make you hireable</li>
-    <li>Looks cool</li>
-  </ol>
-)}
-
-function Footer() {
-  return <footer><small>© 2022 Doorenbosch development. All rights reserved.</small></footer>
-}
-
-const page = (
-  <div>
-    <Navbar />
-    <List />
-    <Footer />
-  </div>
-)
-
-
-
-ReactDOM.render(page
-  , document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"));
